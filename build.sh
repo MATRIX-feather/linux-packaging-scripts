396d7c6c5138b0e3a86dc26d33812a4b50ceec69
MATRIX-feather <midnightcarnival@outlook.com>
2021-10-12 20:23:47 +0800

    Revert "patch"
    This reverts commit efc567db39043b05420529f05ebebdf9afbccd71.

--- build.sh
+++ build.sh
@@ -29,9 +29,9 @@ create_build_directories
 call_quirk init
 
 show_status "Downloading sources"
-#download_repo msa https://github.com/minecraft-linux/msa-manifest.git $(cat msa.commit)
-#download_repo mcpelauncher https://github.com/minecraft-linux/mcpelauncher-manifest.git $(cat mcpelauncher.commit)
-#download_repo mcpelauncher-ui https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git $(cat mcpelauncher-ui.commit)
+download_repo msa https://github.com/minecraft-linux/msa-manifest.git $(cat msa.commit)
+download_repo mcpelauncher https://github.com/minecraft-linux/mcpelauncher-manifest.git $(cat mcpelauncher.commit)
+download_repo mcpelauncher-ui https://github.com/minecraft-linux/mcpelauncher-ui-manifest.git $(cat mcpelauncher-ui.commit)
 
 call_quirk build_start
 
